ec76cc66521c2a575bf0211a680c33e981a470a5
Robson Luan <robsonluan273@hotmail.com>
2022-04-10 14:54:08 -0300

    Works, but the elements arent loaded

--- Main.py
+++ Main.py
@@ -4,10 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get('https://terraria.fandom.com/wiki/Weapons')              #Access the website
-search = driver.find_element(By.CLASS_NAME, "mw-parser-output")     #Find an element
-search.send_keys
+driver.get('https://terraria.fandom.com/wiki/Weapons')                                          #Access the website
+search = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "infocard.clearfix.terraria.compact")))
+texts = []
 
-time.sleep(5)
+for k, i in enumerate(search):
+    lists = WebDriverWait(i, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "itemlist")))
+    items = WebDriverWait(lists, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "i")))
+    for j in items:
+        print(j.text)
+driver.quit()
