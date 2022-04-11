49c38ba4ae9464be8d09db0015f4908d07a44df7
Robson Luan <robsonluan273@hotmail.com>
2022-04-11 01:09:16 -0300

    It works!
    Let's refactor everything now!

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,12 +8,21 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://terraria.fandom.com/wiki/Weapons')                                          #Access the website
+driver.implicitly_wait(5)
+
+try:
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "NN0_TB_DIsNmMHgJWgT7U.XHcr6qf5Sub2F2zBJ53S_"))).click()
+except:
+    pass
+
 search = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "infocard.clearfix.terraria.compact")))
+
 texts = []
 
 for k, i in enumerate(search):
-    lists = WebDriverWait(i, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "itemlist")))
-    items = WebDriverWait(lists, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "i")))
+    lists = WebDriverWait(i, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "itemlist")))
+    items = WebDriverWait(lists, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "i")))
     for j in items:
+        WebDriverWait(lists, 10).until(EC.visibility_of(j))
         print(j.text)
 driver.quit()
--- Setup.py
+++ Setup.py
@@ -1,5 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver                                      # The base of all
+from selenium.webdriver.common.keys import Keys                     # Allow to use keys
+from selenium.webdriver.common.by import By                         # Specify selector
+from selenium.webdriver.chrome.service import Service               # Generates the browser stuff 
+from webdriver_manager.chrome import ChromeDriverManager            # Generates Chrome stuff
+from selenium.webdriver.support.ui import WebDriverWait             # Wait tool
+from selenium.webdriver.support import expected_conditions as EC    # Specified wait tool
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://google.com')
